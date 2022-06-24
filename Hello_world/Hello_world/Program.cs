4a17dd47acd577cb52c1fb94d594d190761d430e
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-06-24 02:48:13 -0600

    create Hello_world

---
+++ Hello_world/Hello_world/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace h_world
+{
+    class my_world
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
